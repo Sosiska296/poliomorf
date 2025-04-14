4d4d61ed4008015e8c94259d93752f2fdae4257f
User_PC-Oren4 <User_PC-Oren4@PC-OREN4>
2025-04-14 19:54:39 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp24/Program.cs
@@ -0,0 +1,27 @@
+﻿ad zero = new ad();
+ad one = new Via();
+ad two = new Moon();
+zero.voice();
+one.voice();
+two.voice();
+class ad
+{
+    public virtual void voice()
+    {
+        Console.WriteLine("IMP");
+    }
+}
+class Via : ad
+{
+    public override void voice()
+    {
+        Console.WriteLine("I will be okay");
+    }
+}
+class Moon : ad
+{
+    public override void voice()
+    {
+        Console.WriteLine("stars");
+    }
+}
